046d346b3ddf15d19e8971a4cb5633f00cff8f62
mitchellpayn <88126584+mitchellpayn@users.noreply.github.com>
2022-09-27 12:54:22 +1000

    CP1401 Prac_02 score_menu

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,13 +3,26 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-# TODO: Fix this!
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+import random
+
+def main():
+    # score = float(input("Enter score: "))
+    # result = determine_score_result(score)
+    # print(f"Your result is {result}")
+    score = random.randint(0, 100)
+    result = determine_score_result(score)
+    print(f"Your result is {result}")
+
+def determine_score_result(score):
+    if score < 0 or score > 100:
+        result = "Invalid score"
+    elif score >= 90:
+        result = "Excellent"
+    elif score >= 50:
+        result = "Passable"
+    else:
+        result = "Bad"
+    return result
+
+
+main()
