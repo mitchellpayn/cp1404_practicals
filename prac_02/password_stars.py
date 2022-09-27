2952186074024670a9cac9cd6938dd134dfe5604
mitchellpayn <88126584+mitchellpayn@users.noreply.github.com>
2022-09-27 12:06:39 +1000

    CP1401 Prac_02 score

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,8 +1,23 @@
-password_min_length = 5
-password = input("enter password:")
-while password != "q":
+"""
+
+"""
+
+def main():
+    password = get_password()
+    print_name_in_stars(password)
+
+
+def print_name_in_stars(password):
+    print('*' * len(password))
+
+
+def get_password():
+    password_min_length = 5
+    password = input("enter password:")
     if len(password) < password_min_length:
         print("Password not long enough")
-    else:
-        print('*' * len(password))
-    password = input("enter password")
+        password = input("enter password")
+    return password
+
+
+main()
